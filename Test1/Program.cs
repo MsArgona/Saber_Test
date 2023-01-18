321e59dd22a6a11c585e6c7f2ff19d8a2b8f6a9e
MsArgona <elenapichugina2012@gmail.com>
2023-01-18 15:25:47 +0400

    Добавьте файлы проекта.

---
+++ Test1/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Xml.Linq;
+using System.Reflection;
+
+class Test
+{
+    public static void Main()
+    {
+        const string PATH = @"L:\TestSaber\test.csv";
+        int count = 5;
+
+        //Creating a List
+        ListRand listRand = new ListRand();
+        listRand.Count = count;
+
+        //Filling the List with Nodes, where "i" is the Data
+        for (int i = 0; i < count; i++)
+            AddNodeTo(listRand, i.ToString());
+
+        //Rand field initialization
+        ListNode next = listRand.Head;
+        while (next != null)
+        {
+            next.Rand = GetRandomNodeFrom(listRand);
+            next = next.Next;
+        }
+
+        PrintList(listRand.Head);
+
+        //Serialize
+        FileStream s = File.Create(PATH);
+        listRand.Serialize(s);
+
+        //Deserialize
+        s = File.OpenRead(PATH);
+        ListRand newListRand = new ListRand();
+        newListRand.Deserialize(s);
+
+        PrintList(newListRand.Head);
+    }
+
+    class ListNode
+    {
+        public ListNode Prev;
+        public ListNode Next;
+        public ListNode Rand; // произвольный элемент внутри списка
+        public string Data;
+    }
+
+    class ListRand
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serialize(FileStream s)
+        {
+            using (s)
+            {
+                //Rand index, Data
+                ListNode next = Head;
+                while (next != null)
+                {
+                    AddText(s, (GetNodeNumIn(this, next.Rand).ToString() + "," + next.Data.ToString() + "\n"));
+                    next = next.Next;
+                }
+            }
+        }
+
+        public void Deserialize(FileStream s)
+        {
+            //<Index in the list, Tuple<Rand index, Rand ListNode>>
+            Dictionary<int, Tuple<int, ListNode>> nodeData = new Dictionary<int, Tuple<int, ListNode>>();
+
+            int index = 0;
+
+            //Read data from csv file, fill Dictionary and fill list with nodes data without rand nodes
+            using (s)
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                int readLen;
+                while ((readLen = s.Read(b, 0, b.Length)) > 0)
+                {
+                    string[] tmpStr = (temp.GetString(b, 0, readLen - 1)).Split(new char[] { '\n', '\r', ',' });
+
+                    for (int i = 0; i < tmpStr.Length; i += 2)
+                    {
+                        //delete all newlines
+                        tmpStr[i] = tmpStr[i].Replace("\n", "").Replace("\r", "");
+                        tmpStr[i + 1] = tmpStr[i + 1].Replace("\n", "").Replace("\r", "");
+
+                        //Fill list with nodes data without rand nodes
+                        AddNodeTo(this, tmpStr[i + 1]);
+
+                        //Fill Dictionary
+                        nodeData.Add(index, new(Int32.Parse(tmpStr[i]), Tail));
+
+                        //Console.WriteLine(index + ", " + nodeData[index]);
+                        index++;
+                    }
+                }
+            }
+
+            // Fill list with rand node data from Dictionary
+            ListNode next = Head;
+            index = 0;
+            while (next != null)
+            {
+                int randNodeIndex = nodeData[index].Item1;
+                next.Rand = nodeData[randNodeIndex].Item2;
+
+                next = next.Next;
+                index++;
+            }
+        }
+    }
+
+    private static int GetNodeNumIn(ListRand listRand, ListNode desiredNode)
+    {
+        int index = 0;
+
+        ListNode next = listRand.Head;
+        while (next != null)
+        {
+            if (next == desiredNode)
+                return index;
+
+            index++;
+            next = next.Next;
+        }
+
+        return -1;
+    }
+
+    private static ListNode? GetRandomNodeFrom(ListRand listRand)
+    {
+        Random rnd = new Random();
+        int randIndex = rnd.Next(listRand.Count); //0..4
+        int curIndex = 0;
+
+        ListNode next = listRand.Head;
+        while (next != null)
+        {
+            if (curIndex == randIndex)
+                return next;
+
+            curIndex++;
+            next = next.Next;
+        }
+
+        return null;
+    }
+
+    private static void PrintList(ListNode startNode)
+    {
+        ListNode next = startNode;
+        while (next != null)
+        {
+            Console.WriteLine(next.Data);
+            next = next.Next;
+        }
+    }
+
+    private static void AddNodeTo(ListRand listRand, string data)
+    {
+        ListNode newNode = new ListNode();
+        newNode.Data = data;
+
+        if (listRand.Head == null)
+            listRand.Head = newNode;
+        else
+        {
+            listRand.Tail.Next = newNode;
+            newNode.Prev = listRand.Tail;
+        }
+        listRand.Tail = newNode;
+    }
+
+    private static void AddText(FileStream s, string value)
+    {
+        byte[] info = new UTF8Encoding(true).GetBytes(value);
+        s.Write(info, 0, info.Length);
+    }
+}
